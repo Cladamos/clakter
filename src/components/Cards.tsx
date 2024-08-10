05820d1b82fc1fa18ac683a0f1e6f3325c954ba0
Cladamos <caliskangokberk@gmail.com>
2024-08-10 16:55:19 +0300

    Add class filter to spells

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,7 +3,7 @@ import axios from "axios"
 import { useQuery } from "@tanstack/react-query"
 import { Container, Grid, Skeleton, Alert, Input, Group, Pagination, Button, Stack, Popover, MultiSelect, Text, Divider } from "@mantine/core"
 import { IconAdjustmentsHorizontal, IconExclamationCircle, IconSearch } from "@tabler/icons-react"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import SpellModal from "./Modals/SpellModal"
 import { useDisclosure } from "@mantine/hooks"
 import CreateSpellModal from "./Modals/CreateSpellModal"
@@ -20,11 +20,22 @@ export type FetchAllSpellsResponseBody = {
     level: string
   }[]
 }
+type FetchClassSpellsResponseBody = {
+  results: {
+    index: string
+  }[]
+}
 
 async function fetchAllSpels() {
   const response = await axios<FetchAllSpellsResponseBody>("https://www.dnd5eapi.co/api/spells")
   return response.data
 }
+
+async function fetchSpellsByClass(className: string) {
+  const response = await axios<FetchClassSpellsResponseBody>(`https://www.dnd5eapi.co/api/classes/${className}/spells`)
+  return response.data.results
+}
+
 function Cards() {
   const query = useQuery({
     queryKey: ["spells"],
@@ -36,11 +47,29 @@ function Cards() {
   const [openedCreateSpellModal, { open: openCreateSpellModal, close: closeCreateSpellModal }] = useDisclosure(false)
   const [activePage, setPage] = useState(1)
   const [levelFilter, setLevelFilter] = useState<string[]>([])
-  // const [classFilter, setClassFilter] = useState<string[]>([])
+  const [filteredClassSpells, setFilteredClassSpells] = useState<{ index: string }[]>([])
+  const [classFilter, setClassFilter] = useState<string[]>([])
   // const [schoolFilter, setSchoolFilter] = useState<string[]>([])
   const cardsPerPage = 12
 
   const { createdSpells, setCreatedSpells } = useCreatedSpells()
+
+  useEffect(() => {
+    if (classFilter.length === 0) {
+      setFilteredClassSpells([])
+      return
+    }
+
+    const fetchClassSpells = async () => {
+      const allClassSpells = await Promise.all(classFilter.map((c) => fetchSpellsByClass(c.toLowerCase())))
+      const combinedClassSpells = allClassSpells.flat()
+      setFilteredClassSpells(combinedClassSpells)
+    }
+
+    fetchClassSpells()
+  }, [classFilter])
+
+  useEffect(() => setPage(1), [levelFilter, classFilter])
 
   function handleCreateSpell(spell: createdSpell) {
     setCreatedSpells((spells) => [...spells, spell])
@@ -90,13 +119,33 @@ function Cards() {
       return { index: c.name.toLowerCase(), name: c.name, level: c.level }
     }),
   ]
+
+  console.log()
+
   const data = mergedSpells.filter((curr) => curr.name.toLowerCase().includes(input.toLowerCase()))
-  const filteredData =
-    levelFilter.length == 0 ? data : data.filter((d) => levelFilter.some((l) => (l == "Cantrip" ? "0" == d.level : l.slice(0, 1) == d.level)))
+
+  const classFilteredData =
+    classFilter.length > 0
+      ? [
+          ...data.filter((s) => filteredClassSpells.some((c) => c.index.toLowerCase() === s.index)),
+          ...createdSpells
+            .filter((s) => {
+              return classFilter.some((cls) => s.classes.toLowerCase().split(", ").includes(cls.toLowerCase()))
+            })
+            .map((s) => {
+              return { index: s.name.toLowerCase(), name: s.name, level: s.level }
+            }),
+        ]
+      : data
+
+  const levelFilteredData =
+    levelFilter.length > 0
+      ? classFilteredData.filter((d) => levelFilter.some((l) => (l == "Cantrip" ? "0" == d.level : l.slice(0, 1) == d.level)))
+      : classFilteredData
 
   const indexOfLastCard = activePage * cardsPerPage
   const indexOfFirstCard = indexOfLastCard - cardsPerPage
-  const currentCards = filteredData.slice(indexOfFirstCard, indexOfLastCard)
+  const currentCards = levelFilteredData.slice(indexOfFirstCard, indexOfLastCard)
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setInput(event.target.value)
@@ -152,18 +201,18 @@ function Cards() {
                     value={levelFilter}
                     onChange={setLevelFilter}
                   />
-                  {/* <Divider />
+                  <Divider />
                   <MultiSelect
                     label="Filter by class"
                     comboboxProps={{ withinPortal: false }}
                     size="sm"
                     clearable
                     searchable
-                    data={["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]}
+                    data={["Bard", "Cleric", "Druid", "Paladin", "Ranger", "Sorcerer", "Warlock", "Wizard"]}
                     value={classFilter}
                     onChange={setClassFilter}
                   />
-                  <Divider />
+                  {/* <Divider />
                   <MultiSelect
                     label="Filter by school"
                     comboboxProps={{ withinPortal: false }}
@@ -189,11 +238,11 @@ function Cards() {
             Create Your Own Spells
           </Button>
           <Pagination
-            total={Math.ceil(filteredData.length / cardsPerPage)}
+            total={Math.ceil(levelFilteredData.length / cardsPerPage)}
             value={activePage}
             onChange={setPage}
             size="sm"
-            disabled={filteredData.length < cardsPerPage}
+            disabled={levelFilteredData.length < cardsPerPage}
           />
         </Stack>
       </Container>
