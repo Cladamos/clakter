812433c0ceef6906518819b48e03525f1f6e57b5
Cladamos <caliskangokberk@gmail.com>
2025-02-11 20:13:06 +0300

    Fix top margins are not matched on every page

--- src/components/CharacterSheet/CharacterSheet.tsx
+++ src/components/CharacterSheet/CharacterSheet.tsx
@@ -97,7 +97,7 @@ function CharacterSheet() {
         <HpModal opened={openedHpModal} close={closeHpModal} key={c.id} />
         <PersonalDetailsModal opened={openedPersonalDetailsModal} close={closePersonalDetailsModal} />
         <DiceRollModal opened={openedDiceRollModal} close={closeDiceRollModal} input={rollInput} />
-        <Container size="lg" mt={90}>
+        <Container size="lg" mt={100}>
           <Group gap="lg">
             {isMobile ? (
               <></>
